a3f6ecb5145fe3f0725e353027ec0571c07e508c
益宏 劉 <107111224@mail.oit.edu.tw>
2021-04-20 15:11:35 +0800

    2-a完成

--- 109_2Mid/Sample1Com.aspx.cs
+++ 109_2Mid/Sample1Com.aspx.cs
@@ -8,8 +8,8 @@ using System.Web.UI.WebControls;
 namespace _109_2Mid {
     public partial class Sample1Com : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            if (Request.Form.Get("tb_Acc.Text") != "") 
+            if (Request.Form.Get("tb_Acc.Text") != "") ;
         }
-        return tb_Acc.Text += Request.Form.Get("tb_Acc")<br>
+        
     }
 }
